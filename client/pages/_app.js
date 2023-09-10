f53f114c068a5be073092c72f78bdb8f055751e3
azad71 <mdmamun732@gmail.com>
2023-09-10 20:02:58 +0600

    added navbar on client service

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
+import httpClient from "../api/http-client";
+import Header from "../components/header";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = httpClient(appContext.ctx);
+
+  const { data } = await client.get("/api/users/currentuser");
+  const pageProps =
+    (await appContext?.Component?.getInitialProps?.(appContext.ctx)) ?? {};
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
