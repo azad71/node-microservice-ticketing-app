5bbdd484854d7fa17376c1c1777734be07d6c438
azad71 <mdmamun732@gmail.com>
2023-09-13 23:31:11 +0600

    added tickets service boilerplate

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,14 @@
+import request from "supertest";
+import { app } from "../../app";
+
+describe("Ticket Service - Create New Ticket", () => {
+  it("has a route handler listening to /api/tickets for post requests", async () => {});
+
+  it("can only be accessible to signed in user", async () => {});
+
+  it("returns an error if an invalid title is provided", async () => {});
+
+  it("returns an error if an invalid price is provided", async () => {});
+
+  it("creates new ticket with valid input", async () => {});
+});
